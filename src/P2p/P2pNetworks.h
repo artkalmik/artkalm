0acc2cbd112c037de349670bec5102778435c38c
artkalm <artkalm@icloud.com>
2021-03-05 17:21:04 +0300

    Init

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x22, 0x16, 0xde, 0x7a, 0x2c, 0x22, 0x4e, 0x4f, 0x54, 0x45, 0x02, 0x12, 0x10, 0x11, 0x16, 0x22 } };
+}
